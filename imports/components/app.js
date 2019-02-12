5fe818a6848f2e517119977deb6cc26662750fec
Kurt Meyerhofer <k@kcmr.io>
2019-02-12 12:35:58 -0700

    Modifies root of project for redux integration.

--- imports/components/app.js
+++ imports/components/app.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 // React Components
+import ErrorContainer from './errorContainer.js';
 import Upload from './upload.js';
 import Folder from './folder.js';
 
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <h1>hello this works</h1>
-          <Route exact path='/' component={Upload} />
-          <Route path='/:folderID' component={Folder} />
-        </div>
-      </BrowserRouter>
-    );
+      <ErrorContainer>
+        <h1>hello this works</h1>
+        <Route exact path='/' component={Upload} />
+        <Route path='/:folderID' component={Folder} />
+      </ErrorContainer>
+    )
   }
 }
