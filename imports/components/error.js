38aacdbcdb70df3b0a7aac57685286467751e6ca
Kurt Meyerhofer <k@kcmr.io>
2019-02-12 17:27:37 -0700

    Adds error display functionality.

--- imports/components/error.js
+++ imports/components/error.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class Error extends Component {
   render() {
     return (
-      <div>
+      <div onClick={() => this.props.removeSelf(this.props.id)}>
         <h3>An error occurred:</h3>
         <p>{this.props.message}</p>
+        <p>Id: {this.props.id}</p>
       </div>
     );
   }
---
+++ imports/components/errorContainer.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Error from './error.js';
+import { removeErrorById } from '../redux/actions/errorActions.js';
+
+class ErrorContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.removeSelf = this.removeSelf.bind(this);
+  }
+
+  removeSelf = (errorId) => {
+    this.props.removeErrorById(errorId);
+  }
+
+  renderEachError = (errors) => {
+    if (errors) {
+      return errors.map((error) => {
+        return (
+          <Error
+            key={error.id}
+            message={error.message}
+            id={error.id}
+            removeSelf={this.removeSelf}
+          />
+        );
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>This is the Error Container component!</h2>
+        <ul>
+          {this.renderEachError(this.props.errors)}
+        </ul>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeErrorById: (errorId) => {
+      dispatch(removeErrorById(errorId));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ErrorContainer);
