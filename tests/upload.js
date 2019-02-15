c9e82f2bd53c101091e995a06072efca9d40eed6
Kurt Meyerhofer <k@kcmr.io>
2019-02-15 10:23:46 -0700

    WIP.

---
+++ tests/upload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import assert from 'chai';
+import Upload from '../imports/components/upload.js';
+
+describe('Upload', () => {
+  it('should render', () => {
+    const item = shallow(<Upload />);
+    console.log('hi');
+    assert(item.hasClass());
+  });
+});
