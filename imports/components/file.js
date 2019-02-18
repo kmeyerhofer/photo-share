eb2229df8a30c52b9f4f6487624553d265cedda5
Kurt Meyerhofer <k@kcmr.io>
2019-02-18 13:59:03 -0700

    WIP.

--- imports/components/file.js
+++ imports/components/file.js
@@ -7,6 +7,7 @@ export default class File extends Component {
   state = {
     fileData: '',
     loaded: false,
+    decrypted: false,
   };
 
   // retrieveFile = () => {
@@ -17,21 +18,23 @@ export default class File extends Component {
   // }
 
   componentDidUpdate = () => {
-    if (!this.state.loaded && this.props.passwordEntered) {
+    if (!this.state.loaded) {
       this.loadEachFileIntoState();
+    } else if (this.props.passwordEntered) {
+      // decrypt
     }
   }
 
   loadEachFileFromServ = async () => { // get a better understanding of this
     const file = this.props.fileData;
-    console.log(file);
+    // console.log(file);
     // let fileData = '';
     // let base64EncodedFile;
     // for (let i = 0; i < files.length; i++) {
     const base64EncodedFile = await callWithPromise('fileLoad', file.fileLocation); // returns a promise, await on promise
-    const fileData = decrypt(base64EncodedFile, this.props.password, file.salt, file.iv);
+    // const fileData = decrypt(base64EncodedFile, this.props.password, file.salt, file.iv);
     // }
-    return fileData;
+    return base64EncodedFile;
   }
 
   loadEachFileIntoState = () => {
@@ -40,14 +43,33 @@ export default class File extends Component {
     });
   }
 
-  render() {
+  decryptFile = () => {
+    const file = this.props.fileData;
+    this.setState({
+      fileData: decrypt(this.state.fileData, this.props.password, file.salt, file.iv),
+      decrypted: true,
+    });
+  }
+
+  renderFile = () => {
     window.URL = window.URL || window.webkitURL;
-    // this.retrieveFile();
-    // console.log(this.state.fileData);
     const strippedBase64 = this.state.fileData.split(',')[1].replace(/\s/g, '');
     const blob = blobUtil.base64StringToBlob(strippedBase64);
     return (
       <img src={window.URL.createObjectURL(blob)} />
     );
+  }
+
+  render() {
+    if (this.props.loaded && this.props.passwordEntered && this.props.decrypted) {
+      // render
+      return this.renderFile();
+    } else if (this.props.loaded && this.props.passwordEntered) {
+      // decrypt
+      this.decryptFile();
+    } else if (!this.state.loaded) {
+      // load files
+      this.loadEachFileIntoState();
+    }
   }
 }
