77d13c440f1df2217a2874326f4c8ff9d954fb38
Kurt Meyerhofer <k@kcmr.io>
2019-02-20 22:08:24 -0700

    Adds Meteor Method parameter checks.

--- server/main.js
+++ server/main.js
@@ -1,14 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import fse from 'fs-extra';
 import MongoFiles from '../imports/api/mongoFiles.js';
-
-/* eslint-disable meteor/audit-argument-checks */
 
 Meteor.startup(() => {
 });
 
 Meteor.methods({
   fileUpload(fileData, encryptedFile) {
+    check(fileData, { url: String, fileLocation: String, fileName: String });
+    check(encryptedFile, String);
     const dirLocation = `${process.env.PWD}/tmp`;
     const fullFileLocation = `${dirLocation}/${fileData.fileLocation}`; // use this endpoint for file loading
     fse.outputFile(fullFileLocation, encryptedFile, { encoding: 'base64' }, function (err) {
@@ -17,6 +18,7 @@ Meteor.methods({
   },
 
   fileLoad(fullFileLoc) {
+    check(fullFileLoc, String);
     const dirLocation = `${process.env.PWD}/tmp`;
     const fullFilePath = `${dirLocation}/${fullFileLoc}`;
     const loadFile = Meteor.wrapAsync(fse.readFile);
@@ -26,6 +28,7 @@ Meteor.methods({
 });
 
 Meteor.publish('files', function(folderURL) {
+  check(folderURL, String);
   return MongoFiles.find({
     url: `${folderURL}`,
   });
