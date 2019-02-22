85614a1a69ff55670a814025f9f7bb4c352672a5
Kurt Meyerhofer <k@kcmr.io>
2019-02-22 13:36:28 -0700

    Adds app test.

---
+++ tests/app.tests.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../imports/components/app.js';
+
+if (Meteor.isClient) {
+  configure({ adapter: new Adapter() });
+
+  describe('<App />', () => {
+    it('should have message text within h1', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('h1').text()).to.equal('hello this works');
+    });
+  });
+}
