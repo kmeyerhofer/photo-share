58c1f4777e91e4e54ca895a444a5322d609a8bfb
kmeyerhofer <k@kcmr.io>
2019-02-27 11:50:16 -0700

    Removes extra spaces.

--- imports/api/commentSchema.js
+++ imports/api/commentSchema.js
@@ -5,14 +5,14 @@ const commentSchema = new SimpleSchema({
   comments: {
     type: Array,
   },
-  'comments.$' : {
+  'comments.$': {
     type: Object,
   },
-  'comments.$.author' : {
+  'comments.$.author': {
     type: String,
-    optional: true
+    optional: true,
   },
-  'comments.$.comment' : {
+  'comments.$.comment': {
     type: String,
   },
 });
