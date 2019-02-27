8b0027843e9d892567e47650c43e873f64bf0aba
kmeyerhofer <k@kcmr.io>
2019-02-27 11:48:09 -0700

    Fixes eslint errors on file.

--- imports/components/commentForm.js
+++ imports/components/commentForm.js
@@ -1,36 +1,24 @@
-/* eslint-disable react/no-find-dom-node, react/no-string-refs */
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 export default class CommentForm extends Component {
-
   state = {
-    author: "",
-    comment: "",
+    author: '',
+    comment: '',
   };
-
-  // updateAuthor = (event) => {
-  //   this.setState({author:event.targe})
-  // }
 
   handleFormSubmit = (event) => {
     event.preventDefault();
-
     const authorInput = this.state.author;
     const commentInput = this.state.comment;
-
     const commentData = Object.freeze({ author: authorInput, comment: commentInput });
-
     this.props.saveComment(commentData);
   }
-
 
   render() {
     return (
       <form onSubmit={this.handleFormSubmit}>
-        <input type="text" ref="author" placeholder="enter your name (optional)" onChange={ (evt) => {this.setState({author: evt.target.value});}} />
-        <br />
-        <textarea ref="comment" placeholder="comment..." onChange={ (evt) => {this.setState({comment: evt.target.value});}} />
+        <input type="text" placeholder="enter your name (optional)" onChange={(evt) => { this.setState({ author: evt.target.value }); }} />
+        <textarea placeholder="comment..." onChange={(evt) => { this.setState({ comment: evt.target.value }); }} />
         <button type="submit">submit</button>
       </form>
     );
