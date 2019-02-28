f981d6a1b794946bb7c510471f066612714e3cb1
kmeyerhofer <k@kcmr.io>
2019-02-27 21:27:28 -0700

    Fixes most tests.

--- tests/unit/passwordEncrypt.tests.js
+++ tests/unit/passwordEncrypt.tests.js
@@ -10,7 +10,7 @@ if (Meteor.isClient) {
   describe('<Password /> (Encrypt)', () => {
     it('should have password input placeholder', () => {
       const wrapper = shallow(<Password />);
-      expect(wrapper.find('input[type="password"]').prop('placeholder')).to.equal('Password');
+      expect(wrapper.find('input[type="password"]').prop('placeholder')).to.equal('Enter Password');
     });
   });
 }
